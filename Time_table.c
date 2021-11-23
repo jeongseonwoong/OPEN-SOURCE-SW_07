e7920f1c99a7261c7839edfcdccef11465f1941a
KyeongTaek <tommyholic@naver.com>
2021-11-23 15:57:36 +0900

    수정사항 : Time_table.c 파일의 프로그램 상 전체적인 구조를 정하고, 필요없는 부분은 제거하고, 몇몇 부분에 대한 주석을 달음.

--- Time_table.c
+++ Time_table.c
@@ -3,10 +3,6 @@
 
 #define MAX 50
 
-/*struct {
-    char arr[6][MAX];//subject name for each class(6 subjects. X for 'No subject')
-}time[13];*/
-
 void print_date(int i)
 {
     if (i == 0)printf("Mon ");
@@ -23,7 +19,10 @@ void  time_table()
     char arr[13][6][MAX];
     if((in=fopen("time_table.txt", "r"))==NULL))//When the file doesn't exists
     {
-      printf("wrong");//get arguments. Make time table out of these arguments, and save then in files.
+      printf("No time table available!");
+      
+      printf("Press 1 if you want to end the program\n");
+      printf("Press 2 if you want to edit the original one or make a new one\n");
     }
     else
     {
@@ -31,29 +30,56 @@ void  time_table()
       {
           for (int j = 0; j < 6; j++)
           {
-              fscanf(in, "%s", arr[i][j]);
+              fscanf(in, "%s", arr[i][j]);//read from time_table.txt
           }
       }
+      
       printf("time/day ");
       for (int i = 0; i < 6; i++)
       {
-          print_date(i);
+          print_date(i);//print weekday and weekend
       }
       printf("\n");
+      
       for (int i = 0; i < 13; i++)
       {
-          if(i==0)printf("%d%d~%d ", 0, 9, 10);
-          else printf("%d~%d ", i + 9, i + 10);
+          if(i==0)printf("%d%d~%d ", 0, 9, 10);//print time
+          else printf("%d~%d ", i + 9, i + 10);//print time
+          
           for (int j = 0; j < 6; j++)
           {
-              printf("%s ", arr[i][j]);
+              printf("%s ", arr[i][j]);//print text read from time_table.txt(class name)
           }
           printf("\n");
       }
+      
+      printf("Press 2 if you want to edit the original one or make a new one\n");
+      printf("Press 3 if you want to use the original one\n");
+
       fclose(in);
 
-      //read file
-
     }
-
+    FILE* out;
+    
+    printf(">> ");
+    
+    int rst = 0;
+    scanf("%d", &rst);
+    
+    if (rst == 2)//edit original or make new. get arguments. Make time table out of these arguments, and save then in time_table.txt
+    {
+        out=fopen("time_table.txt", "w")
+    }
+    else //end program || use the original
+    {
+        if (rst == 1)
+        {
+            printf("See you later. . .");
+        }
+        else if (rst == 3)
+        {
+            printf("time table saved. . .\n\n");
+            printf("See you later. . .");
+        }  
+    }
 }
