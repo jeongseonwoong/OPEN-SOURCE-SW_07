9bea53ef5d0ac57552f29c2387e70255932b7c75
JJW <gurugikr75@gmail.com>
2021-11-23 15:55:22 +0900

    adding time_function to the to_do function

--- TO_do.c
+++ TO_do.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #define SIZE 100
 
 struct list {
@@ -16,23 +17,27 @@ void to_do(FILE *fp)
 {
 	FILE* FP = fp;
 	list* now = (list*)malloc(sizeof(struct list));
-	while (1)
+	while (1)//switch case 이용해서 1. 이전 정보 불러오기, 2. 새로 작성하기
 	{
+		time_t t = time(NULL);
 		char sub[100];
 		char thg[100];
 		int page;
+		struct tm tm = *localtime(&t);
 
 		printf("과목(없다면 x를 입력하세요) : ");
 		scanf_s("%s", &sub, SIZE);
 		strcpy(now->subject, sub);
 		if (sub[0] == 'x')
 			break;
-		printf("할일 : ");
+		printf("할일(공백대신'_'를 사용하세요) : ");
 		scanf_s("%s", &thg, SIZE);
 		strcpy(now->things, thg);
 		printf("마지막으로 공부한 페이지 : ");
 		scanf_s("%d", &page);
 		now->l_page = page;
+		fprintf(FP, "%d-%d-%d\n",
+			tm.tm_year+1900, tm.tm_mon+1, tm.tm_mday);
 		fprintf(FP, "과목 : %s\n", sub);
 		fprintf(FP, "할일 : %s\n", thg);
 		fprintf(FP, "마지막 페이지 : %d\n", page);
@@ -45,12 +50,33 @@ void to_do(FILE *fp)
 
 }
 
+void choice(FILE *fp)
+{
+	int num = 0;
+	char buffer[1000] = { 0, };
+	printf("다음 중 하고 싶은 작업의 번호를 입력하세요\n1. 이전 정보 불러오기\n2. 새로 작성하기\n");
+	scanf_s("%d", &num);
+	switch (num)
+	{
+	case 1:
+		fread(buffer, 1, 1000, fp);
+		printf("%s", buffer);
+		break;
+	case 2:
+		to_do(fp);
+		break;
+	default:
+		break;
+	}
+}
+
 int main()
 {
 	FILE* fp;
-	fp = fopen("To_do_things.txt", "w");
+	fp = fopen("To_do_things.txt", "a+");
 
-	to_do(fp);
+
+	choice(fp);
 
 	fclose(fp);
 	return 0;
