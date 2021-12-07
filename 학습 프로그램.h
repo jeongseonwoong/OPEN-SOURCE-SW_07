e2756785832caf2b7ca9b8bd8c8749fd83f566bf
jeongseonwoong <tjsdnd1219@naver.com>
2021-12-07 16:07:25 +0900

    파일 나누

---
+++ 학습 프로그램.h
@@ -0,0 +1,17 @@
+include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<time.h>
+#include<Windows.h>
+#define MAX 100
+int dday();
+void calender();
+int suneong_1();
+int suneong_2();
+void 이미지나간수능날짜();
+void 얼마나남았습니다();
+void 데이터저장여부();
+void 임의의날짜입력();
+void 몇학년도수능();
+void 데이터x();
+void 데이터o();
