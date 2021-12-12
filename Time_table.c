d05bf641a79b67471b8a508eb8692e734eff968e
KyeongTaek <tommyholic@naver.com>
2021-12-12 21:42:14 +0900

    수정사항 : Time_table.h 파일에 함수 print_date 원형 변경, 함수 find_subject, show_time_table, get_date, edit_time_table 원형 추가, 헤더파일 <string.h>, <stdbool.h> 추가

--- Time_table.c
+++ Time_table.c
@@ -1,86 +1,312 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "Time_table.h"
 
-#define MAX 50
+#define MAX 100
+int size=0;
 
-void print_date(int i)
+struct Node{
+  char subject[MAX];
+}index[MAX];
+
+void print_date(int i, int iter)
 {
-    if (i == 0)printf("Mon ");
-    else if (i == 1)printf("Tue ");
-    else if (i == 2)printf("Wed ");
-    else if (i == 3)printf("Thu ");
-    else if (i == 4)printf("Fri ");
-    else if (i == 5)printf("Sat ");
+    iter = iter - 2;
+    if (i == 0)
+    {
+        printf("Mon");
+        for (int j = 0; j < iter-1; j++)
+        {
+            printf(" ");
+        }
+        printf("|");
+    }
+    else if (i == 1)
+    {
+        printf("Tue");
+        for (int j = 0; j < iter; j++)
+        {
+            printf(" ");
+        }
+        printf("|");
+    }
+    else if (i == 2)
+    {
+        printf("Wed");
+        for (int j = 0; j < iter; j++)
+        {
+            printf(" ");
+        }
+        printf("|");
+    }
+    else if (i == 3)
+    {
+        printf("Thu");
+        for (int j = 0; j < iter; j++)
+        {
+            printf(" ");
+        }
+        printf("|");
+    }
+    else if (i == 4)
+    {
+        printf("Fri");
+        for (int j = 0; j < iter; j++)
+        {
+            printf(" ");
+        }
+        printf("|");
+    }
+    else if (i == 5)
+    {
+        printf("Sat");
+        for (int j = 0; j < iter; j++)
+        {
+            printf(" ");
+        }
+        printf("|");
+    }
+}
+
+void find_subject()
+{
+    FILE* fp;
+    char arr[13][6][MAX] = { 0, };
+    int idx = 0;
+    fp = fopen("Time_table_.txt", "rb");
+    if (fp == NULL)
+    {
+        printf("Wrong");
+        return 0;
+    }
+    else
+    {
+        for (int i = 0; i < 13; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                fscanf(fp, "%s", arr[i][j]);
+            }
+        }
+        for (int i = 0; i < 13; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                bool arg_not_in = true;
+
+                for (int k = 0; k < 10; k++)//check if the subject is in the structure
+                {
+                    if (strcmp(index[k].subject, arr[i][j]) != 0)//not in specific array
+                    {
+                        continue;
+                    }
+                    else//in specific array
+                    {
+                        arg_not_in = false;//check that the argument is in a specific array
+                        break;
+                    }
+                }
+                if (arg_not_in == true)//if the argument is not in a specific array
+                {
+                    /*for (int p = 0; p < strlen(arr[i][j]); p++)
+                    {
+                        index[idx].subject[p] = arr[i][j][p];
+                    }*/
+                    strcpy(index[idx].subject, arr[i][j]);
+                    idx++;
+                }
+            }
+        }
+        fclose(fp);
+        size = idx;
+    }
+}
+
+void show_time_table(char arr[][6][MAX])
+{
+    printf("time/day |");
+    for (int i = 0; i < 6; i++)
+    {
+        print_date(i, 22);//print weekday and weekend
+    }
+    printf("\n");
+
+    for (int i = 0; i < 13; i++)
+    {
+        if (i == 0)printf(" %02d~%d   |", 9, 10);//print time
+        else printf(" %02d~%d   |", i + 9, i + 10);//print time
+
+        for (int j = 0; j < 6; j++)
+        {
+            printf("%22s| ", arr[i][j]);//print text read from time_table.txt(class name)
+        }
+        printf("\n");
+    }
+}
+
+int get_date(char ch[])
+{
+    if (strcmp(ch, "Mon")==0)return 0;
+    else if (strcmp(ch, "Tue") == 0)return 1;
+    else if (strcmp(ch, "Wed") == 0)return 2;
+    else if (strcmp(ch, "Thu") == 0)return 3;
+    else if (strcmp(ch, "Fri") == 0)return 4;
+    else if (strcmp(ch, "Sat") == 0)return 5;
+    else
+    {
+        get_date(ch);
+    }
+}
+
+void edit_time_table(FILE* fp, char arr[][6][MAX])
+{
+    char day[10], class_name[MAX];
+    int Day, start_time, end_time;
+
+    char newarr[13][6][MAX] = { 0, };
+ANOTHER:
+    for (int i = 0; i < 13; i++)
+    {
+        for (int j = 0; j < 6; j++)
+        {
+            for (int k = 0; k < strlen(arr[i][j]); k++)
+            {
+                newarr[i][j][k] = arr[i][j][k];
+            }
+        }
+    }
+
+    printf("Type in the day you want to edit(ex. Mon)\n");
+    printf("or 'next' for next>>");
+    scanf("%s", &day);
+    while (strcmp(day, "next") != 0)
+    {
+        printf("Starting time>>");
+        scanf("%d", &start_time);
+
+        printf("Ending time>>");
+        scanf("%d", &end_time);
+
+        printf("Class name>>");
+        scanf("%s", &class_name);
+
+        start_time -= 9;
+        end_time -= 9;
+        Day = get_date(day);
+
+        for (int i = start_time; i < end_time; i++)
+        {
+            for (int j = 0; j < strlen(class_name); j++)
+            {
+                newarr[i][Day][j] = class_name[j];
+            }
+        }
+        printf("Type in the day you want to edit(ex. Mon)\n");
+        printf("or 'next' for next>>");
+        scanf("%s", &day);
+    }
+    printf("[Original version]\n");
+    show_time_table(arr);
+
+    printf("\n[Edited version]\n");
+    show_time_table(newarr);
+
+    printf("Type in 1 if you want to save it\n");
+    printf("and type in 2 if you want to use the original one\n");
+    printf("and type in 3 if you want to take another attempt>>");
+
+    int rst = 0;
+    scanf("%d", &rst);
+    if (rst == 1)
+    {
+        for (int i = 0; i < 13; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                fprintf(fp, "%s\n", newarr[i][j]);
+            }
+        }
+        printf("edited one saved. . .");
+    }
+    else if (rst == 2)
+    {
+        printf("original one saved. . .");
+    }
+    else if (rst == 3)
+    {
+        goto ANOTHER;
+    }
 }
 
 void  time_table()
 {
-    FILE *in;
-    char arr[13][6][MAX];
-    if((in=fopen("time_table.txt", "r"))==NULL)//When the file doesn't exists
+
+    FILE* in;
+    char arr[13][6][MAX] = { 0, };
+AGAIN:
+    if ((in = fopen("Time_table_.txt", "rb")) == NULL)//When the file doesn't exists: make new file
     {
-      printf("No time table available!");
-      
-      printf("Press 1 if you want to end the program\n");
-      printf("Press 2 if you want to edit the original one or make a new one\n");
+        FILE* make;
+        make = fopen("Time_table_.txt", "w");
+
+        char *str = "NONE";
+        for (int i = 0; i < 13; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                fprintf(make, "%s\n", str);
+            }
+        }
+        fclose(make);
+
+        FILE* in;
+        goto AGAIN;
     }
     else
     {
-      for (int i = 0; i < 13; i++)
-      {
-          for (int j = 0; j < 6; j++)
-          {
-              fscanf(in, "%s", arr[i][j]);//read from time_table.txt
-          }
-      }
-      
-      printf("time/day ");
-      for (int i = 0; i < 6; i++)
-      {
-          print_date(i);//print weekday and weekend
-      }
-      printf("\n");
-      
-      for (int i = 0; i < 13; i++)
-      {
-          if(i==0)printf("%d%d~%d ", 0, 9, 10);//print time
-          else printf("%d~%d ", i + 9, i + 10);//print time
-          
-          for (int j = 0; j < 6; j++)
-          {
-              printf("%s ", arr[i][j]);//print text read from time_table.txt(class name)
-          }
-          printf("\n");
-      }
-      
-      printf("Press 2 if you want to edit the original one or make a new one\n");
-      printf("Press 3 if you want to use the original one\n");
+        for (int i = 0; i < 13; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                fscanf(in, "%s", arr[i][j]);//read from the Time_table_.txt
+            }
+        }
+        show_time_table(arr);
 
-      fclose(in);
+        printf("\n[PROGRAM]>>Press 1 if you want to edit the original one or make a new one\n");
+        printf("[PROGRAM]>>Press 2 if you want to use the original one\n");
+        printf("[PROGRAM]>>Press 0 if you want to end the program\n");
+
+        fclose(in);
 
     }
     FILE* out;
-    
-    printf(">> ");
-    
+
+    printf("[ME]>> ");
+
     int rst = 0;
     scanf("%d", &rst);
-    
-    if (rst == 2)//edit original or make new. get arguments. Make time table out of these arguments, and save then in time_table.txt
+
+    if (rst == 1)//edit. get arguments. Make time table out of these arguments, and save then in Time_table_.txt
     {
-        out = fopen("time_table.txt", "w");
+        out = fopen("Time_table_.txt", "w");
+        ftell(out);
+        edit_time_table(out, arr);
         fclose(out);
-    }
-    else //end program || use the original
-    {
-        if (rst == 1)
+
+        find_subject();
+        printf("\n\n");
+        for (int i = 0; i < size; i++)
         {
-            printf("See you later. . .");
+            printf("%02d. %s\n", i+1, index[i].subject);
         }
-        else if (rst == 3)
-        {
-            printf("time table saved. . .\n\n");
-            printf("See you later. . .");
-        }  
+    }
+    else if (rst == 2)//use the original
+    {
+        printf("[PROGRAM]>>time table saved. . .\n\n");
+        printf("[PROGRAM]>>See you later. . .");
+    }
+    else if (rst == 0)//end the program
+    {
+        printf("[PROGRAM]>>See you later. . .");
     }
 }
