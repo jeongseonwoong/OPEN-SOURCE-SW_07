6b6185f764486be432ed3f8f221c6b63b66197e2
KyeongTaek <tommyholic@naver.com>
2021-12-12 21:47:44 +0900

    수정사항 : Time_table.h 파일에 함수 print_date 원형 변경, 함수 find_subject, show_time_table, get_date, edit_time_table 원형 추가, 헤더파일 <string.h>, <stdbool.h> 추가

--- Time_table.h
+++ Time_table.h
@@ -1,9 +1,10 @@
-#ifndef TIME_TABLE_C
-#define TIME_TABLE_C
-
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 
-#endif
-
-void print_date(int);
+void print_date(int, int);
+void find_subject();
+void show_time_table(char arr[][6][100]);
+int get_date(char ch[]);
+void edit_time_table(FILE *fp, char arr[][6][100]);
 void time_table();
