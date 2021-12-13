a779ae49e79760c25a904840a9f96c64473a9d0d
KyeongTaek <tommyholic@naver.com>
2021-12-13 19:55:10 +0900

    수정사항 : Time_table.h파일로 구조체 옮긴 형태로 수정. 추가로 구현한 함수 헤더 추가.

--- Time_table.h
+++ Time_table.h
@@ -1,9 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <Windows.h>
 
+#ifndef TIME_TABLE_H
+#define TIME_TABLE_H
+
+struct Node {
+  char subject[100];
+}index[100];
+
+#endif
+
+void print_subject(char arr[], int max);
+void print_table_format(int size);
+int get_max(char arr[][6][100]);
 void print_date(int, int);
-void find_subject();
+int find_subject();
 void show_time_table(char arr[][6][100]);
 int get_date(char ch[]);
 void edit_time_table(FILE *fp, char arr[][6][100]);
