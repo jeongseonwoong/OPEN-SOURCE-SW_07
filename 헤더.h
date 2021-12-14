d1c7605334eefdd9a1ba449d3f5254af39a32e4b
yhk24356 <sangmin9101@naver.com>
2021-12-14 14:36:48 +0900

    헤더 파일로 나눔

---
+++ 헤더.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+#pragma warning(disable:4996)
+
+void my_grade();
+void put_data();
+int timesum(int, int);
+float GPAsum(int, int, float);
+void grade_manage();
+void grade_check(int, int);
