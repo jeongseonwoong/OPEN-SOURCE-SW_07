301aae2a08e0a55477257614c6305a22f7281c3c
KyeongTaek <tommyholic@naver.com>
2021-12-15 05:01:03 +0900

    수정사항 : interface_word 함수 원형 인자 변경

--- Time_table.h
+++ Time_table.h
@@ -3,29 +3,44 @@
 #include <stdbool.h>
 #include <Windows.h>
 
+#define MAX 100
+#define TIME 1000
+
 #ifndef TIME_TABLE_H
 #define TIME_TABLE_H
 
 struct Node {
-    char subject[100];
-}index[100];
+    char subject[MAX];
+}index[MAX];
 
 #endif
 
-void interface_word(char arr1[], char arr2[]);
+void interface_word(char, char);
 
-void print_subject(char arr[], int max);
-void print_table_format(int size);
-int get_max(char arr[][6][100]);
+void print_subject(char, int);
+
+void print_table_format(int);
+
+int get_max(char arr[][6][MAX]);
+
 void print_date(int, int);
-void interface_choice(int choice_num, int choice_length, char arr[][100]);
+
+void interface_choice(int, char arr[][MAX]);
+
 void interface_one(char arr[]);
+
 void interface_two(char arr[]);
+
 void interface_three(char arr[]);
+
 void interface_zero(char arr[]);
+
 int find_subject();
-void show_time_table(char arr[][6][100], int size);
+
+void show_time_table(char arr[][6][MAX], int);
+
 int get_date(char ch[]);
-void edit_time_table(FILE* fp, char arr[][6][100]);
+
+void edit_time_table(FILE*, char arr[][6][MAX]);
 
 void time_table();
