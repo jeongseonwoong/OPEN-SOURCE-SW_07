992df5683f1841e935e4f00274f2318823a8b4e9
jeongseonwoong <tjsdnd1219@naver.com>
2021-12-14 20:51:51 +0900

    메인 화면 종료 추가

--- main.c
+++ main.c
@@ -1,39 +1,40 @@
 #include"헤더.h"
 
-int first()
-{
-	int option;
-	option=main_screen();
-	return option;
-}
-
 int main()
 {
-	int option;
-	option=first();
-	if (option == 1)
+	int option=8,select;
+	select=maintitle();
+	while (option != 7&&select!=2)
 	{
-		dday();
-	}
-	else if (option == 2)
-	{
-		dday2();
-	}
-	else if (option == 3)
-	{
+		option = main_screen(select);
+		if (option == 1)
+		{
+			dday();
+		}
+		else if (option == 2)
+		{
+			dday2();
+		}
+		else if (option == 3)
+		{
 
-	}
-	else if (option == 4)
-	{
-		grade_main();
-	}
-	else if (option == 5)
-	{
+		}
+		else if (option == 4)
+		{
+			grade_main();
+		}
+		else if (option == 5)
+		{
 
-	}
-	else if (option == 6)
-	{
-		calender();
+		}
+		else if (option == 6)
+		{
+			calender();
+		}
+		else if (option == 7)
+		{
+			break;
+		}
 	}
 
 }
